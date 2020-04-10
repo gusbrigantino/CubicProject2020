da78523f2507c90c57c4a71bb674f2f8af613517
augustbrigantino <gus@atollogy.com>
2020-04-10 08:46:54 -0700

    added timer for gui file, not great way to keep screen displayed.

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 #imports
 import socket	
 import sys
+import time
 
 #const vars 
 portNumber = 8080	
@@ -14,56 +15,74 @@ balanceStatusIndex = 3
 
 dataIndex = 0
 
-delimeter = ", "
+delimiter = ','
 dataEnd = b'\0'
 
-#program
+guiScreenDelay = 3000                                                       #3 seconds
 
-guiSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           #create socket
-serverAddress = (IPAdress, portNumber)                                  #create socket info with IP address and port #
-guiSocket.bind(serverAddress)                                           #bind
+def main():
+    guiSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           #create socket
+    serverAddress = (IPAdress, portNumber)                                  #create socket info with IP address and port #
+    guiSocket.bind(serverAddress)                                           #bind
 
-guiSocket.listen(16)                                                    #listen TODO: unsure what argument does/is
+    guiSocket.listen(16)                                                    #listen TODO: unsure what argument does/is
 
-while True: 
-    
-    #TODO: Launch GUI IDLE screen 
-
-    connection, clientAddress = guiSocket.accept()	                    #connection made with client (val)
-    
-    try:
-        #Incoming data in order: "<Acct Name>, <Acct Balance>, <Acct Found Status>, <Acct Balance Status>"
-        #-----------------------------------------------------------------------------------------------#
-
-        rawBytes = connection.recv(bufferSize)                          #get all of the incoming data (size of buffer)
-
-        cleanBytes = rawBytes.split(dataEnd)                            #find the null terminator that ends the desired data in the buffer
-
-        strData = cleanBytes[dataIndex].decode("utf-8")                 #deconde to string type and keep whatever is in from of the null terminator
+    while True: 
         
-        data = strData.split(delimeter)                                 #split data into vars
+        #TODO: Launch GUI IDLE screen 
 
-                                                                        #set new vars
-        acctName = data[nameIndex]
-        acctBalance = data[balanceIndex]
-        acctFoundStatus = int(data[foundStatusIndex])
-        acctBalanceStatus = int(data[balanceStatusIndex])
+        connection, clientAddress = guiSocket.accept()	                    #connection made with client (val)
+        
+        try:
+            #Incoming data in order: "<Acct Name>, <Acct Balance>, <Acct Found Status>, <Acct Balance Status>"
+            #-----------------------------------------------------------------------------------------------#
 
+            rawBytes = connection.recv(bufferSize)                          #get all of the incoming data (size of buffer)
 
-        if(not acctFoundStatus):
-            #TODO: Launch GUI NO ACCT screen
-            print("ACCT NOT FOUND")
-        elif(not acctBalanceStatus):
-            #TODO: Launch GUI NO BALANCE screen 
-            print("NOT ENOUGH ACCT BALANCE")
-        else:
-            #TODO: Launch GUI ACCEPTED screen
-            print("ACCT ACCEPTED")
+            cleanBytes = rawBytes.split(dataEnd)                            #find the null terminator that ends the desired data in the buffer
 
-        #TODO: Add timer to keep screen up approx 5secs?
-    finally:
-        connection.close()                                              #close connection
+            strData = cleanBytes[dataIndex].decode("utf-8")                 #deconde to string type and keep whatever is in from of the null terminator
+            
+            data = strData.split(delimiter)                                 #split data into vars
 
+                                                                            #set new vars
+            acctName = data[nameIndex]
+            acctBalance = data[balanceIndex]
+            acctFoundStatus = int(data[foundStatusIndex])
+            acctBalanceStatus = int(data[balanceStatusIndex])
 
 
+            if(not acctFoundStatus):
+                #TODO: Launch GUI NO ACCT screen
+                print("ACCT NOT FOUND")
+            elif(not acctBalanceStatus):
+                #TODO: Launch GUI NO BALANCE screen 
+                print("NOT ENOUGH ACCT BALANCE")
+            else:
+                #TODO: Launch GUI ACCEPTED screen
+                print("ACCT ACCEPTED")
 
+            Timer(guiScreenDelay)                                           #delays to keep screen displayed
+                                                                            #will catch up if a new beacon arrives will still displaying 
+                                                                            #as long as there is enough space in buffer
+                                                                            #TODO: should open gate after GUI display completes
+                                                                            #maybe find a better way to keep screen up using GUI library
+        finally:
+            connection.close()                                              #close connection
+
+
+def Timer(milliseconds):
+    startTime = int(round(time.time()) * 1000)
+    timerFlag = True
+
+    while(timerFlag):
+        millisecondsPassed = (int(round(time.time()) * 1000) - startTime)
+        if(millisecondsPassed >= milliseconds):
+            timerFlag = False
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
