ebb6bbe5b598e49fd9333dd0013d05f5165d0315
sskiki1412 <62625388+sskiki1412@users.noreply.github.com>
2020-04-13 14:31:40 -0600

    Add files via upload
    Reads output of a Linux Command

---
+++ cmd_test.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+ using namespace std;
+
+string GetStdoutFromCommand(string cmd) {
+
+    string data;
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+    cmd.append(" 2>&1");
+
+    stream = popen(cmd.c_str(), "r");
+    if (stream) {
+    while (!feof(stream))
+    if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+    pclose(stream);
+    }
+    return data;
+    }
+
+    int main (){
+
+    //string ls = GetStdoutFromCommand("ls -la");
+    string ls = GetStdoutFromCommand("ls");
+
+    cout << "LS: " << ls << endl;
+
+    return 0;
+    }
