bd385cdd776e9e68ea22f522909cef547a7f1e29
eramirezvilla <47194852+eramirezvilla@users.noreply.github.com>
2020-04-15 20:00:02 -0700

    Selective pairing by ble addr

--- BLEService.cpp
+++ BLEService.cpp
@@ -2,18 +2,45 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <string.h>
 #include <iostream>
+#include <vector>
+#include <sstream>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
+
 //#include "BLEDevices.h"
 
 
 using namespace std;
 
+vector<string> foundAddresses;
+
 void BLEReset(){
     system("sudo hciconfig hci0 down");
     system("sudo hciconfig hci0 up");
+}
+
+//parses the terminal output by newline char first
+//then parses those lines by space to separate the name and addr
+void parseToVector(string terminalOutput) {
+    stringstream ss(terminalOutput);
+    string to;
+    if(!terminalOutput.empty())
+    {
+        while(getline(ss,to,'\n')){
+           stringstream to_ss(to);
+           string finalAddress;
+           if(!to.empty()){
+               while(getline(to_ss,finalAddress,' ')){
+                   if(finalAddress.size() == 17){
+                   foundAddresses.push_back(finalAddress);
+                   }
+               }
+           }
+        }
+    }
 }
 
 string GetStdoutFromCommand(string cmd) {
@@ -34,13 +61,23 @@ string GetStdoutFromCommand(string cmd) {
     return data;
     }
 
-    int main (){
-    
+int main (){
+    string desiredAddr = "C9:0E:DB:EA:12:02";
+    string connectCommand = "sudo gatttool -b ";
+    const char *command;
+    const char *connect = "connect";
+
     string cmnd = GetStdoutFromCommand("sudo timeout -s INT 5s hcitool lescan");
     
-    
+    parseToVector(cmnd);
 
-    cout << "output: \n" << cmnd << endl;
+    for(int i = 0; i < foundAddresses.size(); i++){
+        if(desiredAddr.compare(foundAddresses[i]) == 0){
+            connectCommand += foundAddresses[i] + " -I";
+            command = connectCommand.c_str();
+            system(command);
+        }
+    }
 
     return 0;
     }
