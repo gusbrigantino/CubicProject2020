b747c0a9ae7a77f2dd11184bd5dfcdf1c3cdbb96
augustbrigantino <gus@atollogy.com>
2020-04-17 10:06:35 -0700

    gui updates and images

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@
 import socket	
 import sys
 import time
+import threading
 import tkinter as tkr
 
 #const vars 
@@ -22,38 +23,41 @@ dataEnd = b'\0'
 guiScreenDelay = 3000                                                       #3 seconds
 
 def main():
-#    guiSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           #create socket
-#    serverAddress = (IPAddress, portNumber)                                  #create socket info with IP address and port #
-#    guiSocket.bind(serverAddress)                                           #bind
 
+#   SOCKET INIT
+#    guiSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           #create socket
+#    serverAddress = (IPAddress, portNumber)                                 #create socket info with IP address and port #
+#    guiSocket.bind(serverAddress)                                           #bind
 #    guiSocket.listen(16)                                                    #listen TODO: unsure what argument does/is
 
+
+#   TKINTER INIT
+    tk = tkr.Tk()
+
+    screenWidth = tk.winfo_screenwidth()
+    screenHeight = tk.winfo_screenheight()
+
+    tk.title("San Diego Metro Station")
+    tk.bind("<F11>", lambda event: tk.attributes("-fullscreen", not tk.attributes("-fullscreen")))
+    tk.bind("<Escape>", lambda event: tk.attributes("-fullscreen", False))
+    tk.attributes('-fullscreen', True)
+
+#   CANVAS INIT
+    canvas = tkr.Canvas(tk, width = screenWidth, height = screenHeight, bg = 'white')
+    canvas.grid()
+
+#   CANVAS IMG
+    cubicImg = tkr.PhotoImage(file='cubicImg.png')
+    canvasCubicImg = canvas.create_image((screenWidth - cubicImg.width()), (screenHeight - cubicImg.height()), image=cubicImg, anchor='nw')
+    
+#   TKINTER UPDATE
+    tk.update_idletasks()
+    tk.update()
+    i = 0
     while True: 
-        
-        #TODO: Launch GUI IDLE screen 
-        tk = tkr.Tk()
-        tk.attributes('-fullscreen', True)
-        tk.title("San Diego Metro Station")
-
-        screenWidth = tk.winfo_screenwidth()
-        screenHeight = tk.winfo_screenheight()
-
-        canvas = tkr.Canvas(tk, width = screenWidth, height = screenHeight, bg = 'white')
-        canvas.grid()
-
-        cubicImg = tkr.PhotoImage(file='cubic.png')
-
-        canvas.create_image(10, 10, image=cubicImg, anchor='se')
-        
-
-        #canvas.create_rectangle(0, 0, screenWidth, screenHeight, fill = 'red') 
-        #canvas.create_text(450, 450, text = "ACCT NOT FOUND")
-
         tk.update_idletasks()
         tk.update()
-
-
-        Timer(5000)
+        Timer(500)
 
 #        connection, clientAddress = guiSocket.accept()	                    #connection made with client (val)
         
@@ -62,15 +66,17 @@ def main():
             #-----------------------------------------------------------------------------------------------#
 
 #            rawBytes = connection.recv(bufferSize)                          #get all of the incoming data (size of buffer)
-
+#            print(type(rawBytes))
 #            cleanBytes = rawBytes.split(dataEnd)                            #find the null terminator that ends the desired data in the buffer
 
 #            strData = cleanBytes[dataIndex].decode("utf-8")                 #decode the string type and keep whatever is in front of the null terminator
             
-            strData = "Venkat,34.0,1,1"
+            strData = ["Venkat,34.0,1,1", "Venkat,34.0,0,1", "Venkat,34.0,1,0", "Venkat,34.0,1,1"]
 
-            data = strData.split(delimiter)                                 #split data into vars
-
+            data = strData[i].split(delimiter)                                 #split data into vars
+            i = i + 1
+            if(i >= 4):
+                i = 0
                                                                             #set new vars
             acctName = data[nameIndex]
             acctBalance = data[balanceIndex]
@@ -79,30 +85,46 @@ def main():
 
 
             if(not acctFoundStatus):
-                #TODO: Launch GUI NO ACCT screen
-                #canvas.create_rectangle(0, 0, 1000, 1000, fill = 'red') 
-                #canvas.create_text(450, 450, text = "ACCT NOT FOUND")
-                print("ACCT NOT FOUND")
+                print("NOT FOUND")
+                invalidImg = tkr.PhotoImage(file='X.png')
+
+                canvasInvalidImg = canvas.create_image((invalidImg.width() / 4), 
+                    (screenHeight - (invalidImg.height() + (invalidImg.height() / 4))), 
+                    image=invalidImg, anchor='nw') 
+
             elif(not acctBalanceStatus):
-                #TODO: Launch GUI NO BALANCE screen
-                #canvas.create_rectangle(0, 0, 1000, 1000, fill = 'red') 
-                #canvas.create_text(450, 450, text = acctName + ", you do not have sufficient funds. Your balance is $" + acctBalance) 
-                print("NOT ENOUGH ACCT BALANCE")
+                print("NO BALANCE")
+                invalidImg = tkr.PhotoImage(file='X.png')
+
+                canvasInvalidImg = canvas.create_image((invalidImg.width() / 4), 
+                    (screenHeight - (invalidImg.height() + (invalidImg.height() / 4))), 
+                    image=invalidImg, anchor='nw') 
+
             else:
-                #TODO: Launch GUI ACCEPTED screen
-                #canvas.create_rectangle(0, 0, 1000, 1000, fill = 'green') 
-                #canvas.create_text(450, 450, text = "Hello, " + acctName + "! Your remaining balance is $" + acctBalance)
-                print("ACCT ACCEPTED")
-            
+                print("ACCEPTED")
+                invalidImg = tkr.PhotoImage(file='check.png')
+
+                canvasValidImg = canvas.create_image((invalidImg.width() / 4), 
+                    (screenHeight - (invalidImg.height() + (invalidImg.height() / 4))), 
+                    image=invalidImg, anchor='nw') 
+
+
             tk.update_idletasks()
             tk.update()
+            Timer(guiScreenDelay)
             
-            Timer(guiScreenDelay)                                           #delays to keep screen displayed
-                                                                            #will catch up if a new beacon arrives will still displaying 
-                                                                            #as long as there is enough space in buffer
-                                                                            #TODO: should open gate after GUI display completes
-                                                                            #maybe find a better way to keep screen up using GUI library
-                                                                            #TODO: make different delay times for different screens NF: 1s NB: 3s A: 3s
+
+            if(not acctFoundStatus or not acctBalanceStatus):
+                canvas.delete(canvasInvalidImg)
+            else:
+                canvas.delete(canvasValidImg)
+
+            #delays to keep screen displayed
+            #will catch up if a new beacon arrives will still displaying 
+            #as long as there is enough space in buffer
+            #TODO: should open gate after GUI display completes
+            #maybe find a better way to keep screen up using GUI library
+            #TODO: make different delay times for different screens NF: 1s NB: 3s A: 3s
             
 
         finally:
@@ -118,8 +140,6 @@ def Timer(milliseconds):
         millisecondsPassed = (int(round(time.time()) * 1000) - startTime)
         if(millisecondsPassed >= milliseconds):
             timerFlag = False
-
-
 
 
 if __name__ == "__main__":
